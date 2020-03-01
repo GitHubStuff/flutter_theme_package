060cd9bd88ef110c9c58277d4f6e2b8c629a5482
Steven Smith <github@icodeforyou.com>
2020-03-01 10:53:37 -0500

    move network apis to on package

--- lib/flutter_theme_package.dart
+++ lib/flutter_theme_package.dart
@@ -6,7 +6,6 @@ export 'src/interval_text_widget.dart';
 export 'src/maskable_form_field.dart';
 export 'src/mode_theme.dart';
 export 'src/mode_theme_data.dart';
-export 'src/network_apis.dart';
 export 'src/network_error_image.dart';
 export 'src/oval_image_touch_widget.dart';
 export 'src/time_helpers.dart';
